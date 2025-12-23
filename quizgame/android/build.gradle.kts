5aad8bf8c1a8940f1be10c0f054dad47e58ed4e1
Talbi Mohamed Amine <talbimohamedamine6@gmail.com>
2025-12-23 02:15:47 +0100

    changes related to integrating Firebase into the Flutter project.

--- quizgame/android/build.gradle.kts
+++ quizgame/android/build.gradle.kts
@@ -10,7 +10,13 @@ val newBuildDir: Directory =
         .dir("../../build")
         .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
+plugins {
+  // ...
 
+  // Add the dependency for the Google services Gradle plugin
+  id("com.google.gms.google-services") version "4.4.4" apply false
+
+}
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
